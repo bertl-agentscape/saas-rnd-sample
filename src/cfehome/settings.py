4cc93b08e554bcc35f35c9e24de3fe3dfcaf9b94
bertl-agentscape <b.ertl@agentscape.de>
2025-02-04 21:33:57 +0100

    add railway

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -25,7 +25,14 @@ SECRET_KEY = 'django-insecure-8q38jpeg1vf*!ju3wg(7w5c-ar%tf_7xk^%rlldy-1=5lb0f3s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".railway.app" # https://saas.prod.railway.app
+]
+if DEBUG:
+    ALLOWED_HOSTS += [
+        "127.0.0.1",
+        "localhost"
+    ]
 
 
 # Application definition
